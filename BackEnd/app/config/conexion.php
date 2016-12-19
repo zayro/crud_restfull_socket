8f685c9144ad23542df60fc4ae8e48541fd6a65d
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2016-12-19 15:07:47 -0500

    se organizo el codigo de conexion
    utilizando la libreria php-cs-fixer

--- BackEnd/app/config/conexion.php
+++ BackEnd/app/config/conexion.php
@@ -8,36 +8,37 @@ use library\DBMS;
 
 const PI = 3.14;
 
-class connect extends DBMS {	
-	
-	public function __construct() {	
-		
-		$database_type = "mysql";
-		
-		$host= "localhost" ;
-		
-		$database = "prueba";
-		
-		$user = "root";
-		
-		$password = "zayro";
-		
-		$port = "3306";
-	
-		
-		parent::__construct($database_type, $host, $database, $user, $password, $port);
-		
-		$this->Cnxn();
-		
-		print_r($this->getError());
+/**
+ * CLASE GENERAL PRINCIPAL.
+ *
+ * En esta parte nos encargamos de crear los tipos de conexion del proyecto
+ * para poder asi administrar los tipos de permisos de acceso
+ */
 
-		#print 'conexion exitosa' ;
-		
-		
-	}
+class connect extends DBMS
+{
+    public function __construct()
+    {
+        $database_type = 'mysql';
 
-	
+        $host = 'localhost';
+
+        $database = 'prueba';
+
+        $user = 'root';
+
+        $password = 'zayro';
+
+        $port = '3306';
+
+        parent::__construct($database_type, $host, $database, $user, $password, $port);
+
+        $this->Cnxn();
+
+        print_r($this->getError());
+
+//print 'conexion exitosa' ;
+    }
 }
 
-
-#$instance = new connect();
+//$instance = new connect();

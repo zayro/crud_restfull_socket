41883b0b95eb466d729921bb6e1855aa603385b1
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2016-12-19 15:18:40 -0500

    se ajusto la identacion de los archivos en php

--- BackEnd/app/controller/general.php
+++ BackEnd/app/controller/general.php
@@ -1,128 +1,96 @@
 <?php
 
-require_once('../../run.php');
+require_once '../../run.php';
 
 use app\model\general;
 use library\excel;
 use library\csv;
 
-$objeto = new general;
-$excel = new excel;
-$csv = new csv;
+$objeto = new general();
+$excel = new excel();
+$csv = new csv();
 
 $returnValue = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$url = explode("general.php/", trim($returnValue, "/"));
+$url = explode('general.php/', trim($returnValue, '/'));
 $first = array_shift($url);
 $id = array_pop($url);
 $MaxArgs = count($url);
 
-
-
 $request = strtoupper($_SERVER['REQUEST_METHOD']);
 
 switch ($request) {
-	
-	
-	case 'GET':  
-	
-	if(isset($method) and $method == 'all_csv'){	
-		
-		$csv->all_csv($table, $file);		
-		
-	}
+    case 'GET':
 
-	if(isset($method) and $method == 'all_excel'){	
-		
-		$excel->all_csv($table, $file);		
-		
-	}	
-
-	if(isset($method) and $method == 'search'){		
-		
-		$result =  $objeto->search($table, $id);		
-		
-		print ($result);
-		
-	}	
-	
-	if(isset($method) and $method == 'column'){		
-		
-		$result =  $objeto->column($table);		
-		
-		print ($result);
-		
-	}	
-
-	if(isset($table) and !isset($method)){        		
-		
-		$result = $objeto->all($table);		
-		
-		print ($result);
-		
-	}
-	
-	break;
-	
-	
-	case 'POST':
-	
-	$receives = json_decode(file_get_contents('php://input'), true);
-
-	$process = "";	
-	
-	foreach($receives as $key => $val){
-		
-		$process .=  $key.'='."'$val',";
-		
-	}	
-	
-	$data = substr($process,0,-1);	
-	
-	if(isset($method) and $method == 'add'){		
-		
-		print $objeto->add($table, $data);		
-		
-	}	
-	
-	break;
-	
-	
-	case 'PUT':
-	
-	$receives = json_decode(file_get_contents('php://input'), true);
-	
-	$process = "";
-
-	foreach($receives as $key => $val){
-		
-		$process .=  $key.'='."'$val', ";
-		
-	}	
-	
-	$data = substr($process,0,-2);
-	
-	
-	if(isset($method) and $method == 'change'){		
-		
-		print $objeto->change($table, $data, $id);		
-		
-	}
-	
-	
-	break;	
-	
-	case 'DELETE':
-	
-	if(isset($id)){			
-		
-		print $objeto->remove($table, $id);
-		
-		
-	}	
-	
-	break;	
-	
+if (isset($method) and $method == 'all_csv') {
+    $csv->all_csv($table, $file);
 }
 
-?>
+if (isset($method) and $method == 'all_excel') {
+    $excel->all_excel($table, $file);
+}
+
+if (isset($method) and $method == 'search') {
+    $result = $objeto->search($table, $id);
+
+    echo $result;
+}
+
+if (isset($method) and $method == 'column') {
+    $result = $objeto->column($table);
+
+    echo $result;
+}
+
+if (isset($table) and !isset($method)) {
+    $result = $objeto->all($table);
+
+    echo $result;
+}
+
+break;
+
+case 'POST':
+
+$receives = json_decode(file_get_contents('php://input'), true);
+
+$process = '';
+
+foreach ($receives as $key => $val) {
+    $process .= $key.'='."'$val',";
+}
+
+$data = substr($process, 0, -1);
+
+if (isset($method) and $method == 'add') {
+    echo $objeto->add($table, $data);
+}
+
+break;
+
+case 'PUT':
+
+$receives = json_decode(file_get_contents('php://input'), true);
+
+$process = '';
+
+foreach ($receives as $key => $val) {
+    $process .= $key.'='."'$val', ";
+}
+
+$data = substr($process, 0, -2);
+
+if (isset($method) and $method == 'change') {
+    echo $objeto->change($table, $data, $id);
+}
+
+break;
+
+case 'DELETE':
+
+if (isset($id)) {
+    echo $objeto->remove($table, $id);
+}
+
+break;
+}
--- BackEnd/library/excel.class.php
+++ BackEnd/library/excel.class.php
@@ -2,65 +2,48 @@
 
 namespace library;
 
-#require_once('../run.php');
+//require_once('../run.php');
 
 use app\config\connect;
 
-class excel extends connect  {
-		
-	
-	public function exportar($query, $filename){			
-
-
-  #header("Content-Type: application/vnd.ms-excel");
-  header("Content-Type: application/vnd.ms-excel; charset=utf-8");
-  header("Content-type: application/x-msexcel; charset=utf-8");
+class excel extends connect
+{
+public function exportar($query, $filename){
+  //header("Content-Type: application/vnd.ms-excel");
+  header('Content-Type: application/vnd.ms-excel; charset=utf-8');
+  header('Content-type: application/x-msexcel; charset=utf-8');
   header("Content-Disposition: attachment; filename=$filename.xls");
-  header("Pragma: no-cache");
-  header("Expires: 0");
-
+  header('Pragma: no-cache');
+  header('Expires: 0');
 
   $nombres = $this->query($query, 'named');
 
   $fields = array_keys($nombres);
   $count = count($fields);
 
+  echo "<table border='1'>";
 
-  print "<table border='1'>";
+  echo '<tr>';
 
-  print "<tr>";
-
-  for ($i = 0 ; $count>$i; $i++){
-    
-    print "<th>". $fields[$i] . "</th>";
-    
+  for ($i = 0; $count > $i; ++$i){
+    echo '<th>'.$fields[$i].'</th>';
   }
 
-  print "</tr>";
-
-
+  echo '</tr>';
 
   foreach ($this->query($query, 'both') as $row) {
+    echo '<tr>';
 
-    print "<tr>";
-    
-  for ($i = 0 ; $count>$i; $i++){
-    
-    print "<td>". $row[$i]. "</td>";
-    
+  for ($i = 0; $count > $i; ++$i){
+    echo '<td>'.$row[$i].'</td>';
   }	
 
 
-    print "</tr>";	
+    echo '</tr>';
   }
 
-    print "</table>";
-
-      
-    }	
-    
-    
-    
+    echo '</table>';
+    }
   }
 
 /*
